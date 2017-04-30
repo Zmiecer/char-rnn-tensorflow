ecd83721181539b806dee76d61c9a9004edff05f
Dzmitry Bylinovich <dmitrii.bylinovich@tut.by>
2017-04-30 16:41:56 +0300

    CompoundRNN initial commit

--- train.py
+++ train.py
@@ -1,24 +1,30 @@
-from __future__ import print_function
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import numpy as np
 import tensorflow as tf
 
 import argparse
 import time
-import os
 from six.moves import cPickle
 
 from utils import TextLoader
 from model import Model
 
-
 def main():
     parser = argparse.ArgumentParser(
-                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--data_dir', type=str, default='data/tinyshakespeare',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--data_dir', type=str, default='data/input',
                         help='data directory containing input.txt')
     parser.add_argument('--save_dir', type=str, default='save',
                         help='directory to store checkpointed models')
     parser.add_argument('--log_dir', type=str, default='logs',
                         help='directory to store tensorboard logs')
+    parser.add_argument('--experiment_log', type=str, default='experiments.txt',
+                        help='file to store experiment parameters (appended)')
+    parser.add_argument('--loss_log', type=str, default='test_loss.txt',
+                        help='file to store test loss progress (appended)')
     parser.add_argument('--rnn_size', type=int, default=128,
                         help='size of RNN hidden state')
     parser.add_argument('--num_layers', type=int, default=2,
@@ -27,16 +33,18 @@ def main():
                         help='rnn, gru, lstm, or nas')
     parser.add_argument('--batch_size', type=int, default=50,
                         help='minibatch size')
-    parser.add_argument('--seq_length', type=int, default=50,
+    parser.add_argument('--seq_length', type=int, default=30,
                         help='RNN sequence length')
     parser.add_argument('--num_epochs', type=int, default=50,
                         help='number of epochs')
-    parser.add_argument('--save_every', type=int, default=1000,
+    parser.add_argument('--save_every', type=int, default=10000,
                         help='save frequency')
     parser.add_argument('--grad_clip', type=float, default=5.,
                         help='clip gradients at this value')
-    parser.add_argument('--learning_rate', type=float, default=0.002,
+    parser.add_argument('--learning_rate', type=float, default=0.01,
                         help='learning rate')
+    parser.add_argument('--min_lr', type=float, default=1e-5,
+                        help='learning rate lower bound (stopping condition)')
     parser.add_argument('--decay_rate', type=float, default=0.97,
                         help='decay rate for rmsprop')
     parser.add_argument('--output_keep_prob', type=float, default=1.0,
@@ -44,15 +52,23 @@ def main():
     parser.add_argument('--input_keep_prob', type=float, default=1.0,
                         help='probability of keeping weights in the input layer')
     parser.add_argument('--init_from', type=str, default=None,
-                        help="""continue training from saved model at this path. Path must contain files saved by previous training process:
+                        help="""continue training from saved model at this path.
+                       Path must contain files saved by previous training process:
                             'config.pkl'        : configuration;
                             'chars_vocab.pkl'   : vocabulary definitions;
                             'checkpoint'        : paths to model file(s) (created by tf).
-                                                  Note: this file contains absolute paths, be careful when moving files around;
+                                                  Note: this file contains absolute paths,
+                                                  be careful when moving files around;
                             'model.ckpt-*'      : file(s) with model definition (created by tf)
                         """)
+    parser.add_argument('--device', type=str, default='/gpu:0',
+                        help='device')
     args = parser.parse_args()
     train(args)
+    with open(args.experiment_log, mode='a') as logfile:
+        print('\nData: {}, {} layers, hidden state size {}, cell type {}'.format(
+            args.data_dir, args.num_layers, args.rnn_size, args.model
+        ), file=logfile)
 
 
 def train(args):
@@ -63,8 +79,10 @@ def train(args):
     if args.init_from is not None:
         # check if all necessary files exist
         assert os.path.isdir(args.init_from)," %s must be a a path" % args.init_from
-        assert os.path.isfile(os.path.join(args.init_from,"config.pkl")),"config.pkl file does not exist in path %s"%args.init_from
-        assert os.path.isfile(os.path.join(args.init_from,"chars_vocab.pkl")),"chars_vocab.pkl.pkl file does not exist in path %s" % args.init_from
+        assert os.path.isfile(os.path.join(args.init_from,"config.pkl")),\
+            "config.pkl file does not exist in path %s"%args.init_from
+        assert os.path.isfile(os.path.join(args.init_from,"chars_vocab.pkl")),\
+            "chars_vocab.pkl.pkl file does not exist in path %s" % args.init_from
         ckpt = tf.train.get_checkpoint_state(args.init_from)
         assert ckpt, "No checkpoint found"
         assert ckpt.model_checkpoint_path, "No model path found in checkpoint"
@@ -74,13 +92,14 @@ def train(args):
             saved_model_args = cPickle.load(f)
         need_be_same = ["model", "rnn_size", "num_layers", "seq_length"]
         for checkme in need_be_same:
-            assert vars(saved_model_args)[checkme]==vars(args)[checkme],"Command line argument and saved model disagree on '%s' "%checkme
+            assert vars(saved_model_args)[checkme]==vars(args)[checkme],\
+                "Command line argument and saved model disagree on '%s' "%checkme
 
         # open saved vocab/dict and check if vocabs/dicts are compatible
         with open(os.path.join(args.init_from, 'chars_vocab.pkl'), 'rb') as f:
             saved_chars, saved_vocab = cPickle.load(f)
-        assert saved_chars==data_loader.chars, "Data and loaded model disagree on character set!"
-        assert saved_vocab==data_loader.vocab, "Data and loaded model disagree on dictionary mappings!"
+        assert saved_chars == data_loader.chars, "Data and loaded model disagree on character set!"
+        assert saved_vocab == data_loader.vocab, "Data and loaded model disagree on dictionary mappings!"
 
     if not os.path.isdir(args.save_dir):
         os.makedirs(args.save_dir)
@@ -88,14 +107,14 @@ def train(args):
         cPickle.dump(args, f)
     with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'wb') as f:
         cPickle.dump((data_loader.chars, data_loader.vocab), f)
-
     model = Model(args)
 
     with tf.Session() as sess:
         # instrument for tensorboard
         summaries = tf.summary.merge_all()
         writer = tf.summary.FileWriter(
-                os.path.join(args.log_dir, time.strftime("%Y-%m-%d-%H-%M-%S")))
+            os.path.join(args.log_dir, time.strftime("%Y-%m-%d-%H-%M-%S"))
+        )
         writer.add_graph(sess.graph)
 
         sess.run(tf.global_variables_initializer())
@@ -103,37 +122,88 @@ def train(args):
         # restore model
         if args.init_from is not None:
             saver.restore(sess, ckpt.model_checkpoint_path)
+
+        prev_loss = np.Infinity
+        av_train_loss = 0
+        min_test_loss = np.Infinity
+
+        with open(args.loss_log, mode='a') as loss_log:
+            print("\nData: {}, number of layers: {}, hidden state size: {}".format(
+                args.data_dir, args.num_layers, args.rnn_size
+            ), file=loss_log)
         for e in range(args.num_epochs):
-            sess.run(tf.assign(model.lr,
-                               args.learning_rate * (args.decay_rate ** e)))
-            data_loader.reset_batch_pointer()
+            sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
             state = sess.run(model.initial_state)
-            for b in range(data_loader.num_batches):
-                start = time.time()
-                x, y = data_loader.next_batch()
-                feed = {model.input_data: x, model.targets: y}
+            b = 1
+            start = time.time()
+            for x, y, word_lengths in data_loader.iterbatches('train'):
+                feed = {model.input_data: x, model.targets: y, model.word_len: word_lengths}
                 for i, (c, h) in enumerate(model.initial_state):
                     feed[c] = state[i].c
                     feed[h] = state[i].h
-                train_loss, state, _ = sess.run([model.cost, model.final_state, model.train_op], feed)
 
                 # instrument for tensorboard
                 summ, train_loss, state, _ = sess.run([summaries, model.cost, model.final_state, model.train_op], feed)
                 writer.add_summary(summ, e * data_loader.num_batches + b)
+                writer.flush()
+
+                av_train_loss += train_loss
 
                 end = time.time()
-                print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}"
-                      .format(e * data_loader.num_batches + b,
-                              args.num_epochs * data_loader.num_batches,
-                              e, train_loss, end - start))
-                if (e * data_loader.num_batches + b) % args.save_every == 0\
-                        or (e == args.num_epochs-1 and
-                            b == data_loader.num_batches-1):
-                    # save for the last result
-                    checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
-                    saver.save(sess, checkpoint_path,
-                               global_step=e * data_loader.num_batches + b)
-                    print("model saved to {}".format(checkpoint_path))
+                print("{}/{} (file {}/{}, epoch {}/{}), train_loss = {:.3f}, time/batch = {:.3f}".format(
+                    b - data_loader.num_batches*data_loader.file_number, data_loader.num_batches,
+                    data_loader.file_number, data_loader.num_train_files - 1,
+                    e, args.num_epochs - 1, train_loss, end - start
+                ))
+                b += 1
+
+                start = time.time()
+
+            av_train_loss /= b
+
+            # test
+            av_test_loss = 0
+            t = 1
+            start = time.time()
+            for x_test, y_test, word_lengths in data_loader.iterbatches('test'):
+                feed = {model.input_data: x_test, model.targets: y_test, model.word_len: word_lengths}
+                test_loss = sess.run([model.cost], feed)
+                test_loss = test_loss[0]
+                av_test_loss += test_loss
+                end = time.time()
+                print("{}/{} (file {}/{}, epoch {}/{}), test_loss = {:.3f}, time/batch = {:.3f}".format(
+                    t - data_loader.num_batches*(data_loader.file_number - data_loader.num_train_files + 1),
+                    data_loader.num_batches,
+                    data_loader.file_number - data_loader.num_train_files - 1, data_loader.num_test_files - 1,
+                    e, args.num_epochs - 1,
+                    test_loss, end - start
+                ))
+                t += 1
+                start = time.time()
+
+            av_test_loss /= t
+
+            with open(args.loss_log, mode='a') as loss_log:
+                print("Epoch {}, learning_rate = {}, train_loss = {}, test_loss = {}"
+                      .format(e, args.learning_rate, av_train_loss, av_test_loss), file=loss_log)
+
+            if av_test_loss >= prev_loss:
+                args.learning_rate /= 2
+                print(args.learning_rate)
+            if av_test_loss < min_test_loss:
+                checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path,
+                           global_step=e * data_loader.num_batches * data_loader.num_train_files)
+                print("model saved to {}".format(checkpoint_path))
+                min_test_loss = av_test_loss
+            prev_loss = av_test_loss
+
+            print("epoch {} ended".format(e))
+            if args.learning_rate < args.min_lr:
+                print("Minimum reached", args.learning_rate, "<", args.min_lr)
+                break
+
+            data_loader.file_number = 0
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,33 +1,45 @@
-import codecs
 import os
-import collections
-from six.moves import cPickle
-import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+import codecs
+import collections
+import glob
+import numpy as np
+import itertools
+import random
+from six.moves import cPickle
+import time
 
 class TextLoader():
-    def __init__(self, data_dir, batch_size, seq_length, encoding='utf-8'):
+    def __init__(self, data_dir, batch_size, seq_length, encoding='utf-8', train_percentage=0.5):
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.seq_length = seq_length
         self.encoding = encoding
+        self.file_number = 0
+        self.num_of_files = sum(1 for item in glob.iglob(os.path.join(self.data_dir, 'input*.txt')))
+        self.vocabulary_created = False
 
-        input_file = os.path.join(data_dir, "input.txt")
-        vocab_file = os.path.join(data_dir, "vocab.pkl")
-        tensor_file = os.path.join(data_dir, "data.npy")
+        self.num_train_files = int(self.num_of_files * train_percentage)
+        self.num_test_files = self.num_of_files - self.num_train_files
 
+        input_file, vocab_file, tensor_file = self._get_filenames(self.file_number)
         if not (os.path.exists(vocab_file) and os.path.exists(tensor_file)):
-            print("reading text file")
-            self.preprocess(input_file, vocab_file, tensor_file)
+            print("Reading text file")
+            self._preprocess(input_file, vocab_file, tensor_file)
         else:
-            print("loading preprocessed files")
-            self.load_preprocessed(vocab_file, tensor_file)
-        self.create_batches()
-        self.reset_batch_pointer()
+            print("Loading preprocessed files")
+            self._load_preprocessed(vocab_file, tensor_file)
+        self._create_batches()
+        self._reset_batch_pointer()
 
-    def preprocess(self, input_file, vocab_file, tensor_file):
-        with codecs.open(input_file, "r", encoding=self.encoding) as f:
-            data = f.read()
+    def _get_filenames(self, file_number):
+        input_file = os.path.join(self.data_dir, "input{}.txt".format(file_number))
+        vocab_file = os.path.join(self.data_dir, "vocab.pkl")
+        tensor_file = os.path.join(self.data_dir, "data{}.npz".format(file_number))
+        return input_file, vocab_file, tensor_file
+
+    def _create_vocabulary(self, data, vocab_file):
         counter = collections.Counter(data)
         count_pairs = sorted(counter.items(), key=lambda x: -x[1])
         self.chars, _ = zip(*count_pairs)
@@ -35,41 +47,90 @@ class TextLoader():
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
         with open(vocab_file, 'wb') as f:
             cPickle.dump(self.chars, f)
-        self.tensor = np.array(list(map(self.vocab.get, data)))
-        np.save(tensor_file, self.tensor)
 
-    def load_preprocessed(self, vocab_file, tensor_file):
+        self.vocabulary_created = True
+
+    def _preprocess(self, input_file, vocab_file, tensor_file):
+        with codecs.open(input_file, "r", encoding=self.encoding) as f:
+            data = f.read()
+        # self.len_of_words = np.loadtxt(self.data_dir + "/length" + str(self.file_number) + ".txt", dtype=int) + 1
+
+        self._create_vocabulary("abcdefghijklmnopqrstuvwxyzßäöü ", vocab_file)
+
+        words = data.split()
+        self.tensor = []
+        self.word_lengths = []
+        count = len(words)
+        for index, word in enumerate(words):
+            if len(word) < self.seq_length - 1:
+                seq = np.full(self.seq_length, self.vocab[' '])
+                try:
+                    seq[1:len(word) + 1] = np.array(list(map(self.vocab.get, word)))
+                except TypeError:
+                    print(word)
+                    break
+                self.tensor.append(seq)
+                self.word_lengths.append(len(word) + 2)
+                if (index + 1) % 10000 == 0:
+                    print('{}/{} words parsed'.format(index + 1, count))
+        np.savez(tensor_file, tensor=self.tensor, word_lengths=self.word_lengths)
+
+    def _load_preprocessed(self, vocab_file, tensor_file):
         with open(vocab_file, 'rb') as f:
             self.chars = cPickle.load(f)
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
-        self.tensor = np.load(tensor_file)
-        self.num_batches = int(self.tensor.size / (self.batch_size *
-                                                   self.seq_length))
+        data = np.load(tensor_file)
+        self.tensor = data['tensor']
+        self.word_lengths = data['word_lengths']
 
-    def create_batches(self):
-        self.num_batches = int(self.tensor.size / (self.batch_size *
-                                                   self.seq_length))
+    def _create_batches(self):
+        self.num_batches = int(len(self.tensor) / self.batch_size)
 
-        # When the data (tensor) is too small,
-        # let's give them a better error message
+        # When the data (tensor) is too small, let's give them a better error message
         if self.num_batches == 0:
             assert False, "Not enough data. Make seq_length and batch_size small."
 
-        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
-        xdata = self.tensor
-        ydata = np.copy(self.tensor)
-        ydata[:-1] = xdata[1:]
-        ydata[-1] = xdata[0]
-        self.x_batches = np.split(xdata.reshape(self.batch_size, -1),
-                                  self.num_batches, 1)
-        self.y_batches = np.split(ydata.reshape(self.batch_size, -1),
-                                  self.num_batches, 1)
+        dataset = self.tensor[:self.num_batches * self.batch_size]
 
-    def next_batch(self):
-        x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
-        self.pointer += 1
-        return x, y
+        x = np.array(dataset)
+        timer = time.clock()
+        random.shuffle(x)
+        print('{:.2f} seconds for shuffle'.format(time.clock() - timer))
 
-    def reset_batch_pointer(self):
+        y = x.copy().reshape(-1)
+        nil = y[0]
+        y[:-1] = y[1:]
+        y[-1] = nil
+        y = y.reshape(x.shape)
+
+        self.xs = np.split(x, self.num_batches)
+        self.ys = np.split(y, self.num_batches)
+        self.word_lengths = np.array(self.word_lengths)
+        self.wls = np.split(self.word_lengths[:self.num_batches * self.batch_size], self.num_batches)
+
+    def iterbatches(self, mode):
+        while True:
+            if self.pointer >= self.num_batches:
+                self.file_number += 1
+                if (mode == "train" and self.file_number >= self.num_train_files
+                        or mode == "test" and self.file_number >= self.num_of_files):
+                    return
+
+                self._reset_batch_pointer()
+
+                input_file, vocab_file, tensor_file = self._get_filenames(self.file_number)
+                if not (os.path.exists(vocab_file) and os.path.exists(tensor_file)):
+                    print("Reading text file")
+                    self._preprocess(input_file, vocab_file, tensor_file)
+                else:
+                    print("Loading preprocessed files")
+                    self._load_preprocessed(vocab_file, tensor_file)
+                # self._preprocess(input_file, vocab_file, tensor_file)
+                self._create_batches()
+            x, y, word_lengths = self.xs[self.pointer], self.ys[self.pointer], self.wls[self.pointer]
+            self.pointer += 1
+            yield x, y, word_lengths
+
+    def _reset_batch_pointer(self):
         self.pointer = 0
